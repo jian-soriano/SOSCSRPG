8932d1e16f2b5ce688d8b6e41d8e28e3a66100e4
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-04 18:14:58 -0400

    created Player class with properties. \nCreated GameSession class with constructor

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        
+        // constructors
+
+        // properties
+        public string Name { get; set; }
+        public string Class {  get; set; }
+        public int HitPoints {  get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
