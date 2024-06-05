98cabad74cbff08210fe6ec1ff0a459aff0b6f2a
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-05 18:09:25 -0400

    created World class to store Location objects. created WorldFactory class to generate World objects.

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    // internal because it will only be used inside the Engine project
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+
+            newWorld.AddLocation(-2, -1, "Farmer's Field", 
+                "There are rows of corn growing here, with giant rats hiding between them.",
+                "pack://application:,,,/Engine;component/Images/Locations/FarmFields.png");
+            
+            newWorld.AddLocation(-1, -1, "Farmer's House", 
+                "This is the house of your neighbor, Farmer Ted.", 
+                "pack://application:,,,/Engine;component/Images/Locations/Farmhouse.png");
+            
+            newWorld.AddLocation(0, -1, "Home", 
+                "This is your home.", "pack://application:,,,/Engine;component/Images/Locations/Home.png");
+
+            return newWorld; 
+        }
+    }
+}
---
+++ Engine/Models/World.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        // fields
+        private List<Location> _locations = new List<Location>();
+
+        //methods
+        // internal because WorldFactory class is the only other class that should use it
+        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName) 
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoordinate;
+            loc.YCoordinate = yCoordinate;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
+
+        // returns a Location from _locations with the given x/y coordinates, or null if not found
+        public Location LocationAt(int xCoordinate, int yCoordinate)
+        {
+            foreach (Location loc in _locations)
+            {
+                if (loc.XCoordinate == xCoordinate && loc.YCoordinate == yCoordinate) { return loc; }
+            }
+            return null;
+        }
+    }
+}
