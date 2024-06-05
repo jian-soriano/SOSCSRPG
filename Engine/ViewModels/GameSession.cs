ff4d974101c3a3e9ed2ec4c1667896d667552341
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-05 18:13:54 -0400

    replaced hard-coded CurrentLocation instantiation with World and WorldFactory functionality.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
@@ -12,10 +13,12 @@ namespace Engine.ViewModels
         // properties
         public Player CurrentPlayer {  get; set; }
         public Location CurrentLocation { get; set; }
+        public World CurrentWorld { get; set; }
 
         // constructor
         public GameSession()
         {
+            // sets the starting player values
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Jian";
             CurrentPlayer.CharacterClass = "Fighter";
@@ -24,12 +27,12 @@ namespace Engine.ViewModels
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your house.";
-            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
+            // generates the game world
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            // sets the starting location as Home
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
         }
     }
 }
