357d4862502cf8e4ee2e488c967eabf0ca6ce172
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-06 16:54:31 -0400

    added movement methods and relative location booleans for navigation button functionality. implemented INotifyPropertyChanges and added OnPropertyChanged function calls to CurrentLocation

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,17 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
 using Engine.Factories;
+using System.ComponentModel;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
-        // properties
+        // FIELDS
+        private Location _currentLocation;
+        
+        // PROPERTIES
         public Player CurrentPlayer {  get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation 
+        { 
+            get { return _currentLocation; }
+            set
+            {
+                _currentLocation = value;
+                // when CurrentLocation is changed, these will update the MainWindow.xaml file appropriately
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToWest");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToSouth");
+            }
+        }
         public World CurrentWorld { get; set; }
+        
+        // these bools will set visibility of nav buttons to Visible/Hidden if returns true/false
+        public bool HasLocationToNorth
+        {
+            get 
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
 
-        // constructor
+        public bool HasLocationToWest
+        {
+            get 
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+
+        // CONSTRUCTOR
         public GameSession()
         {
             // sets the starting player values
@@ -34,5 +84,36 @@ namespace Engine.ViewModels
             // sets the starting location as Home
             CurrentLocation = CurrentWorld.LocationAt(0, -1);
         }
+
+        // METHODS
+
+        // public because it will be called from the WPFUI/MainWindow.xaml.cs file
+        public void MoveNorth() 
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);         
+        }
+
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+        public void MoveSouth()
+        {
+           CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
     }
 }
