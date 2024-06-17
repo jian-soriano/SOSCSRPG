6141a2a135d0e776ba124954d8277c98955a392c
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-17 12:41:21 -0400

    created Monster class.

---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    internal class Monster : BaseNotificationClass
+    {
+        // FIELDS
+
+        private int _hitPoints;
+
+        // PROPERTIES
+
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            private set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold {  get; private set; }
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        // CONSTRUCTOR
+
+        public Monster(string name, string imageName, 
+            int maximumHitPoints, int hitPoints, 
+            int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = imageName;
+            MaximumHitPoints = maximumHitPoints;
+            HitPoints = hitPoints;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+            Inventory = new ObservableCollection<ItemQuantity>();
+        }
+    }
+}
