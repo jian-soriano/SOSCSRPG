8ab355b0ce8ad8f8d3d8506d46de945dd9700b4a
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-19 15:18:11 -0400

    added monster spawns to WorldFactory.cs.

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -18,6 +18,8 @@ namespace Engine.Factories
             newWorld.AddLocation(-2, -1, "Farmer's Field",
                 "There are rows of corn growing here, with giant rats hiding between them.",
                 "pack://application:,,,/Engine;component/Images/Locations/FarmFields.png");
+
+            newWorld.LocationAt(-2, -1).AddMonster(2, 100);
             
             newWorld.AddLocation(-1, -1, "Farmer's House",
                 "This is the house of your neighbor, Farmer Ted.",
@@ -43,15 +45,19 @@ namespace Engine.Factories
                 "The trees in this forest are covered with spider webs.",
                 "pack://application:,,,/Engine;component/Images/Locations/SpiderForest.png");
 
+            newWorld.LocationAt(2, 0).AddMonster(3, 100);
+
             newWorld.AddLocation(0, 1, "Herbalist's Hut",
                 "You see a small hut, with plants drying from the roof.",
                 "pack://application:,,,/Engine;component/Images/Locations/HerbalistsHut.png");
 
             newWorld.LocationAt(0, 1).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(1));
 
-            newWorld.AddLocation(0, 2, "Herb Garden",
+            newWorld.AddLocation(0, 2, "Herbalist's Garden",
                 "There are many plants here, with snakes hiding behind them.",
                 "pack://application:,,,/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+            newWorld.LocationAt(0, 2).AddMonster(1, 100);
 
             return newWorld; 
         }
