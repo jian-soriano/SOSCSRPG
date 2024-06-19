a7fba021b0b2ff0c662d66d7ccaf543e71cdee9e
Jian Soriano <jsoriano.tdk@gmail.com>
2024-06-19 15:16:57 -0400

    created MonsterEncounter class. added property and methods to Location.cs for storing and handling MonsterEncounters.

---
+++ Engine/Models/MonsterEncounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class MonsterEncounter
+    {
+        // PROPERTIES
+
+        public int MonsterID { get; set; }
+        public int ChanceOfEncountering { get; set; }
+
+        // CONSTRUCTOR
+
+        public MonsterEncounter(int monsterID, int chanceOfEncountering)
+        {
+            MonsterID = monsterID;
+            ChanceOfEncountering = chanceOfEncountering;
+        }
+    }
+}
